b368d23b28223e7927f170c06e64c470a9b69a73
서승 <90320005+seoseuo@users.noreply.github.com>
2022-11-07 16:46:46 +0900

    디렉토리별로 정리 후, 상대 경로로 위치 설정. 드롭 다운 메뉴 구현
    드롭 다운 버튼에서 클릭 시 사이트로 이동하도록 완료.
    드롭 다운 메뉴 행렬이 너무 안맞아서 다시 예쁘게 만들어야 할 듯.

---
+++ src/move.js
@@ -0,0 +1,10 @@
+// 메인 버튼 클릭
+
+var link = ["./index.html","./indexSeo.html","./indexStudy.html","./indexProject.html",
+            "./indexHall.html","./indexAble.html","./indexDefy.html","./indexGrim.html",
+            "./indexLog.html"]; //html 
+
+function goTo(number) {
+    location.href=link[number];
+}
+
