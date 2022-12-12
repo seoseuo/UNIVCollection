f6a8095fc1ae498137dbe9ef69e52e4b4ea35bda
서승 <90320005+seoseuo@users.noreply.github.com>
2022-12-12 18:57:09 +0900

    Add files via upload

--- src/move.js
+++ src/move.js
@@ -1,11 +1,9 @@
 // 메인 버튼 클릭
 
-var link = ["./index.html","./indexSeo.html","./indexStudy.html","./indexProject.html",
-            "./indexHall.html","./indexAble.html","./indexDefy.html","./indexGrim.html",
+var link = ["./index.html","./indexSeo.html","./indexStudy.html","./indexProject.html","","./indexAble.html","./indexDefy.html","./indexGrim.html",
             "./indexLog.html"]; //html 
 
-var mainTolink = ["src/index.html","src/indexSeo.html","src/indexStudy.html","src/indexProject.html",
-            "src/indexHall.html","src/indexAble.html","src/indexDefy.html","src/indexGrim.html",
+var mainTolink = ["src/index.html","src/indexSeo.html","src/indexStudy.html","src/indexProject.html","","src/indexAble.html","src/indexDefy.html","src/indexGrim.html",
             "src/indexLog.html"]; //html 
 
 
